d540ef249f9b38b2ed91611670f27f490fbc3c70
annnafriesen <annafriesen@shaw.ca>
2024-05-29 11:20:30 -0700

    fixed layout on burger menu and added transition

--- Home_Page/burger_menu.js
+++ Home_Page/burger_menu.js
@@ -1,9 +1,9 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+function makeResponsive() {
+  var x = document.getElementById("nav_bar");
+  if (x.className === "full-nav") {
+    x.className = "responsive";
   } else {
-    x.className = "topnav";
+    x.className = "full-nav";
   }
 }
